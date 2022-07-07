2f5bad772dfdbf5dd1dc07f2083f82184d90af12
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-07-07 22:06:22 +0900

    #11726번 다이나믹프로그래밍

---
+++ 11726_problem.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int n;
+int d[1002];
+void tile()
+{
+	for (int i = 3; i <= n; i++)
+	{
+		d[i] = (d[i - 1] + d[i - 2])%10007;
+	}
+}
+
+int main()
+{
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	cin >> n;
+	d[1] = 1;
+	d[2] = 2;
+	tile();
+	cout << d[n] % 10007;
+}
