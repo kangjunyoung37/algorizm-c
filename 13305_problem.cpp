14854d2754ffc847cb2e9ee5b8aeeedc88aebd66
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-08-03 19:50:47 +0900

    #13305번 그리디알고리즘

--- 13305_problem.cpp
+++ 13305_problem.cpp
@@ -1,33 +1,34 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
-
-#define MAX 1000000000 // 10¾ï
-
-long long dist[100001];
-long long price[100001];
-long long N, sum;
-long long greedy;
-
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+
+	int N;
 	cin >> N;
-
-	for (int i = 1; i <= N - 1; i++)
-		cin >> dist[i];
-	
-	for (int i = 1; i <= N; i++)
-		cin >> price[i];
-	
-	greedy = MAX;
-
-	for (int i = 1; i <= N - 1; i++)
+	long long distance[100001];
+	long long prices[100001];
+	for (int i = 1; i < N;i++)
 	{
-		if (price[i] < greedy)
-			greedy = price[i];
-		sum += greedy * dist[i];
+		cin >> distance[i - 1];
+
+	}
+	for (int i = 0; i < N; i++)
+	{
+		cin >> prices[i];
+	}
+	long long result = 0;
+	long long price = prices[0];
+	for (int i = 0; i < N-1;i++)
+	{
+		if (price > prices[i])
+		{
+			price = prices[i];
+		}
+		result  +=  price * distance[i];
 	}
 
-	cout << sum;
 
+	cout << result;
 }
